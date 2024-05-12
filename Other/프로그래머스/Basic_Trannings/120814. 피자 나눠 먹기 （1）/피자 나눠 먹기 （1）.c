00a7d3db84c1b0bb064d5e3169c3429c77464476
최준성 <53941330+Jastes@users.noreply.github.com>
2024-05-12 18:48:57 +0900

    Delete Other directory
    반대로 함;

--- Other/프로그래머스/Basic_Trannings/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int n) {
-    int answer = (n/7) + (n%7 ? 1:0);
-    
-    return answer;
-}
--- Other/프로그래머스/Basic_Trannings/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int a, int b, bool flag) {
-    if(flag)  return a+b;
-    else return a-b;
-}
